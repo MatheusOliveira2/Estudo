f0d1c1e9a0900a62d1b3470932c228061a7ba052
MatheusOliveira2 <matheus.oliveira2@estudante.ufla.br>
2020-03-30 20:51:03 -0300

    Componentes

---
+++ Vue/componentes/components-intro/src/components/Contadores.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="contadores">
+        <app-contador></app-contador>
+        <app-contador></app-contador>
+        <app-contador></app-contador>
+        <app-contador></app-contador>
+    </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+export default {
+    components:{'app-contador':Contador}
+}
+</script>
+
+<style scoped>
+
+</style>
